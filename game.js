bd11bc8988255010eeba44d974544ac64da5ea94
John Gamboa <jayjeex@gmail.com>
2021-05-06 17:56:16 +0800

    project finished

--- game.js
+++ game.js
@@ -1,20 +1,6 @@
 let roundCounter = 1;
 const addRound = () => { roundCounter++; };
 
-const addToHistory = function(roundNumber, playerPlayed, computerPlayed, result){
-    const table = document.querySelector("#historyTable");
-    const row = table.insertRow(-1);
-    const roundCell = row.insertCell(0);
-    const playerCell = row.insertCell(1);
-    const computerCell = row.insertCell(2);
-    const winnerCell = row.insertCell(3);
-
-    roundCell.innerHTML = roundNumber;
-    playerCell.innerHTML = playerPlayed;
-    computerCell.innerHTML = computerPlayed;
-    winnerCell.innerHTML = result;
-};
-
 //create a variable with an array of weapons for computers choices
 const weaponChoices = ['cPlayRock', 'cPlayPaper', 'cPlayScissor'];
 //create a function that will return a random weapon from the array
@@ -101,6 +87,8 @@ let playerScoreCounter = 0;
 //create variable that will keep the computer's score
 let computerScoreCounter = 0;
 //create a function that will add score defending on the round outcome
+const playerScoreElement = document.querySelector('#playerScore');
+const computerScoreElement = document.querySelector('#computerScore');
 const addScore = function(roundResult){
     if(roundResult == 'You win this round!'){
         playerScoreCounter++;
@@ -113,10 +101,17 @@ const addScore = function(roundResult){
         computerScoreCounter += 0;
     }
 
-    const playerScoreElement = document.querySelector('#playerScore');
-    playerScoreElement.innerHTML = playerScoreCounter;
-    const computerScoreElement = document.querySelector('#computerScore');
+    playerScoreElement.innerHTML = playerScoreCounter; 
     computerScoreElement.innerHTML = computerScoreCounter;
+};
+
+const checkScore = function(){
+    if((playerScoreCounter < 5) && (computerScoreCounter < 5)){
+        addRound();
+    } else {
+        fixWeapons();
+        endGame();
+    }
 };
 
 const hideRoundWinner = function(){
@@ -178,6 +173,20 @@ const getRoundNameWinner = function(roundResult){
     return name;
 };
 
+const table = document.querySelector("#historyTable");
+const addToHistory = function(roundNumber, playerPlayed, computerPlayed, result){
+    const row = table.insertRow(-1);
+    const roundCell = row.insertCell(0);
+    const playerCell = row.insertCell(1);
+    const computerCell = row.insertCell(2);
+    const winnerCell = row.insertCell(3);
+
+    roundCell.innerHTML = roundNumber;
+    playerCell.innerHTML = playerPlayed;
+    computerCell.innerHTML = computerPlayed;
+    winnerCell.innerHTML = result;
+};
+
 ////create a function that will enable back all player inputs
 const fixWeapons = function(){
     weapons.forEach(function(weapon){
@@ -214,19 +223,13 @@ const playRound = function(){
     //record it to the history
     setTimeout(() => { addToHistory(roundCounter, playerPlayed, computerPlayed, result); }, 5000);
 
+    //check if the game should still continue, if true run addRound()
+    setTimeout(checkScore, 5050);
+    
+    setTimeout(showRoundCounter, 5100);
     //enable the inputs
-    setTimeout(fixWeapons, 5199);
-    //add +1 to the roundCounter
-    // setTimeout(addRound, 5200); deciding if I need to remove this in this function
+    setTimeout(fixWeapons, 5200);
 };
-
-//create a variable containing an array list of objects with a class name of playerWeapons
-const weaponsOfPlayer = document.querySelectorAll('.playerWeapons');
-
-//adding an event listener for each element inside weaponsOfPlayer
-weaponsOfPlayer.forEach(function(weapon){
-    weapon.addEventListener('click', playRound);
-});
 
 const toggleHowToPlay = function(){
     const htpInfoElement = document.querySelector('#htpInfo');
@@ -245,6 +248,145 @@ const toggleHowToWin = function(){
         htwInfoElement.style.display = 'none';
     }
 }
+
+const titleElement = document.querySelector('#title');
+const hideTitle = function(){
+    titleElement.style.display = 'none';
+};
+
+const hidePlayDiv = function(){
+    playDiv.style.display = 'none';
+};
+
+const howToElement = document.querySelectorAll('.howTo');
+const hideHowToElements = function(){
+    howToElement.forEach(function(e){
+        e.style.display = 'none';
+    });
+};
+
+const roundContainerElement = document.querySelector('#round_container');
+const showRoundContainerElement = function(){
+    roundContainerElement.style.display = 'block';
+};
+const hideRoundContainerElement = function(){
+    roundContainerElement.style.display = 'none';
+};
+
+const roundNumberElement = document.querySelector('#roundNumber');
+const showRoundCounter = function(){
+    roundNumberElement.innerHTML = roundCounter;
+};
+
+const gameContainerElement = document.querySelector('#gameContainer');
+const showGameContainer = function(){
+    gameContainerElement.style.display = 'flex';
+};
+const hideGameContainer = function(){
+    gameContainerElement.style.display = 'none';
+};
+
+const winnerDivElement = document.querySelector('#winnerDiv');
+const showWinnerDivElement = function(){
+    winnerDivElement.style.display = 'flex';
+};
+const hideWinnerDivElement = function(){
+    winnerDivElement.style.display = 'none';
+};
+
+const historyContainerElement = document.querySelector('#history');
+const showHistory = function (){
+    historyContainerElement.style.display = 'flex';
+};
+const hideHistory = function (){
+    historyContainerElement.style.display = 'none';
+};
+
+let winner = null;
+const fetchWinner = function(){
+    if(playerScoreCounter > computerScoreCounter){
+        winner = 'Player';
+    }
+    if(computerScoreCounter > playerScoreCounter){
+        winner = 'Computer';
+    }
+    return winner;
+};
+
+const winnerNameElement = document.querySelector('#winnerName');
+const displayWinner = function(winner){
+    winnerNameElement.innerHTML = winner;
+};
+
+const deleteHistory = function(){
+    let rowCount = table.rows.length;
+    for(let i = rowCount - 1; i > 0; i--){
+        table.deleteRow(i);
+    }
+};
+
+const resetScoreCounter = function(){
+    playerScoreElement.innerHTML = playerScoreCounter; 
+    computerScoreElement.innerHTML = computerScoreCounter;
+};
+
+const returnHome = function(){
+    location.reload();
+    return false;
+};
+
+//start the game
+const playGame = function() {
+    hideTitle();
+    hidePlayDiv();
+    hideHowToElements();
+    showRoundContainerElement();
+    showRoundCounter();
+    showGameContainer();
+};
+
+//end the game
+const endGame = function(){
+    winner = fetchWinner();
+
+    hideRoundContainerElement();
+    hideGameContainer();
+    showWinnerDivElement();
+    displayWinner(winner);
+    showHistory();
+};
+
+//play again
+const playAgain = function(){
+    roundCounter = 1;
+    playerScoreCounter = 0;
+    computerScoreCounter = 0;
+
+    hideWinnerDivElement();
+    hideHistory();
+    deleteHistory();
+
+    showRoundContainerElement();
+    showRoundCounter();
+    showGameContainer();
+    resetScoreCounter();
+};
+
+const playDiv = document.querySelector('#playDiv');
+playDiv.addEventListener('click', playGame);
+
+//create a variable containing an array list of objects with a class name of playerWeapons
+const weaponsOfPlayer = document.querySelectorAll('.playerWeapons');
+//adding an event listener for each element inside weaponsOfPlayer
+weaponsOfPlayer.forEach(function(weapon){
+    weapon.addEventListener('click', playRound);
+});
+
+const menuButton = document.querySelector('#menu');
+menuButton.addEventListener('click', returnHome);
+
+const playAgainButton = document.querySelector('#playAgain');
+playAgainButton.addEventListener('click', playAgain);
 
 const howToPlayElement = document.querySelector('#htpArrow');
 howToPlayElement.addEventListener('click', toggleHowToPlay);
